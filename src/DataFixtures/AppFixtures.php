2f939c63fbd559cbbf1034f40174acebb723e419
Simon <simon@opalsolutions.be>
2020-01-04 00:25:03 +0100

    ajout user

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Team;
+use App\Entity\User;
+use App\Entity\Table;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * Encode password
+     *
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder) 
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        
+        $faker = Factory::create('fr_FR');
+
+        for($u = 0; $u < 5; $u++){
+            $user = New User();
+            $hash = $this->encoder->encodePassword($user, 'password');
+            $user->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword($hash);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
