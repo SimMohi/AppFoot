8e7a2aa95b74ca0e23cad845d17404cacf9fb81d
Simon <simon@opalsolutions.be>
2020-01-04 02:34:54 +0100

    entity division and club

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ApiResource
  */
 class User implements UserInterface
 {
